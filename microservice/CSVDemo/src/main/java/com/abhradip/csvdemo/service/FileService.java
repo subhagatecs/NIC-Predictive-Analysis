2dacc817ada4fe147abbb711503bacaaf22bf34c
Abhradip Saha <abhradip360saha@gmail.com>
2024-05-23 16:34:25 +0530

    Post request csv

---
+++ microservice/CSVDemo/src/main/java/com/abhradip/csvdemo/service/FileService.java
@@ -0,0 +1,11 @@
+package com.abhradip.csvdemo.service;
+
+import org.springframework.web.multipart.MultipartFile;
+
+public interface FileService {
+
+	boolean hasCsvFormat(MultipartFile file);
+
+	void processAndSaveData(MultipartFile file);
+
+}
