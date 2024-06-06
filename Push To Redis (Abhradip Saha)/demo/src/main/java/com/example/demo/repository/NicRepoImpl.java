29d082f9f2e1436d677c0ef3a8cfb7549a14515b
Abhradip Saha <110524706+abhradip-saha@users.noreply.github.com>
2024-06-07 03:01:11 +0530

    Update NicRepoImpl.java

--- Push To Redis (Abhradip Saha)/demo/src/main/java/com/example/demo/repository/NicRepoImpl.java
+++ Push To Redis (Abhradip Saha)/demo/src/main/java/com/example/demo/repository/NicRepoImpl.java
@@ -1,9 +1,5 @@
 package com.example.demo.repository;
-
 import com.example.demo.model.Nic;
-
-import java.util.UUID;
-
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.redis.core.RedisTemplate;
 import org.springframework.stereotype.Repository;
@@ -13,11 +9,10 @@ public class NicRepoImpl implements NicRepo{
     
     @Autowired
     private RedisTemplate redisTemplate;
-    private static final String KEY = UUID.randomUUID().toString() + "27201" + Integer.toString((int) (Math.random() * 900) + 100);
     @Override
     public boolean saveNic(Nic nic){
         try{
-            redisTemplate.opsForHash().put("KEY", KEY, nic);
+            redisTemplate.opsForHash().put("KEY", nic.getKey() , nic);
             return true;
         }catch(Exception e){
             e.printStackTrace();
