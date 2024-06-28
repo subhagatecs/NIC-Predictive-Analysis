4d22180a08fe368676b6847391d5bee2e809d948
Diya Banik <116958230+Banikdiya2002@users.noreply.github.com>
2024-06-29 01:35:27 +0530

    Add files via upload

---
+++ Post Method with CSV (Diya Banik)/src/main/java/com/CSV/CsvProjectNewApplication.java
@@ -0,0 +1,13 @@
+package com.CSV;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CsvProjectNewApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CsvProjectNewApplication.class, args);
+	}
+
+}
